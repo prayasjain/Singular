ee43c2e9abc7eca45e0b8d27f0a87068502ce4a9
prayasjain <prayasjain@github.com>
2020-07-06 04:33:49 +0530

    bug fix android sms permission

--- android/app/src/main/java/com/startup/Singular/SMSPlugin.java
+++ android/app/src/main/java/com/startup/Singular/SMSPlugin.java
@@ -8,6 +8,8 @@ import com.getcapacitor.PluginMethod;
 import android.Manifest;
 import org.json.JSONArray;
 import org.json.JSONException;
+
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.content.ContentResolver;
 import android.database.Cursor;
@@ -17,7 +19,9 @@ import java.util.Map;
 
 import android.util.Log;
 
-@NativePlugin(permissions = { Manifest.permission.READ_SMS, Manifest.permission.RECEIVE_SMS,
+@NativePlugin(
+  permissionRequestCode = 1,
+  permissions = { Manifest.permission.READ_SMS, Manifest.permission.RECEIVE_SMS,
     Manifest.permission.SEND_SMS })
 public class SMSPlugin extends Plugin {
 
@@ -29,12 +33,8 @@ public class SMSPlugin extends Plugin {
       call.resolve(ret);
       return;
     }
-    pluginRequestAllPermissions();
-    if (hasRequiredPermissions()) {
-      call.resolve(ret);
-    } else {
-      call.error("Permission Denied");
-    }
+    requestPermissions(call);
+    //pluginRequestAllPermissions();
   }
 
   private String[] getStringArrayFromJSONArray(JSONArray array) throws JSONException {
@@ -48,76 +48,19 @@ public class SMSPlugin extends Plugin {
     return stringArray;
   }
 
-//  public boolean execute(String action, JSONArray data, CallbackContext callbackContext) {
-//    Log.v("SMSReader", "Called action " + action);
-//    try {
-//      ArrayList<SMS> sms = new ArrayList<SMS>();
-//      switch (action) {
-//        case "permission": {
-//          this.ensurePermissions(this.getStringArrayFromJSONArray(data.getJSONArray(0)), callbackContext);
-//          return true;
-//        }
-//        case "all": {
-//          sms = this.fetchSMS(data.getLong(0), new String[] {}, new String[] {});
-//        }
-//          break;
-//        case "filterbody": {
-//          String[] searchstrings = this.getStringArrayFromJSONArray(data.getJSONArray(1));
-//          if (searchstrings.length > 0) {
-//            sms = this.fetchSMS(data.getLong(0), searchstrings, new String[] {});
-//          }
-//        }
-//          break;
-//        case "filtersenders": {
-//          String[] senderids = this.getStringArrayFromJSONArray(data.getJSONArray(2));
-//          if (senderids.length > 0) {
-//            sms = this.fetchSMS(data.getLong(0), new String[] {}, senderids);
-//          }
-//        }
-//          break;
-//        case "filterbodyorsenders": {
-//          String[] searchstrings = this.getStringArrayFromJSONArray(data.getJSONArray(1));
-//          String[] senderids = this.getStringArrayFromJSONArray(data.getJSONArray(2));
-//          if (searchstrings.length + senderids.length > 0) {
-//            sms = this.fetchSMS(data.getLong(0), searchstrings, senderids);
-//          }
-//        }
-//          break;
-//        default: {
-//          callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.INVALID_ACTION));
-//          return false;
-//        }
-//      }
-//      callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, this.convertToJSONArray(sms)));
-//      return true;
-//    } catch (JSONException e) {
-//      callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.JSON_EXCEPTION, e.getMessage()));
-//      return false;
-//    }
-//  }
+   @Override
+   protected void handleRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+     super.handleRequestPermissionsResult(requestCode, permissions, grantResults);
+     PluginCall savedCall = getSavedCall();
+     JSObject ret = new JSObject();
+     ret.put("success", true);
 
-  // @Override
-  // protected void handleRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-  //   super.handleRequestPermissionsResult(requestCode, permissions, grantResults);
-
-  //   log("handling request perms result");
-  //   PluginCall savedCall = getSavedCall();
-  //   if (savedCall == null) {
-  //     log("No stored plugin call for permissions request result");
-  //     return;
-  //   }
-
-  //   for (int result : grantResults) {
-  //     if (result == PackageManager.PERMISSION_DENIED) {
-  //       savedCall.error("User denied permission");
-  //       return;
-  //     }
-  //   }
-
-  //   if (requestCode == REQUEST_IMAGE_CAPTURE) {
-  //     // We got the permission
-  //   }
-  // }
+     if (!hasRequiredPermissions()) {
+       savedCall.error("permission denied");
+     } else {
+       savedCall.resolve(ret);
+     }
+   }
 
   private JSONArray convertToJSONArray(ArrayList<SMS> sms) throws JSONException {
     JSONArray smsResult = new JSONArray();
@@ -153,9 +96,9 @@ public class SMSPlugin extends Plugin {
           if (accountDataMap.containsKey(accountInfo.getString("account"))) {
             JSObject existingInfo = accountDataMap.get(accountInfo.getString("account"));
             if (existingInfo.getLong("date") > accountInfo.getLong("date")) {
-              System.out.println("skipping old record");
+
             } else {
-              System.out.println("updating account record");
+
               accountDataMap.put(accountInfo.getString("account"), accountInfo);
 
             }
@@ -169,8 +112,6 @@ public class SMSPlugin extends Plugin {
       for(JSObject jsObject : accountDataMap.values()) {
         outputData.put(String.valueOf(jsObject.get("id")), jsObject);
       }
-      System.out.println(outputData);
-      System.out.println("came till here....");
       call.resolve(outputData);
     } catch (JSONException e) {
       call.error("Some error Occurred");
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBq374GQb7ao-D4JI2W5S_V3PG0U7EY3Ks",
+    authDomain: "moneyapp-63c7a.firebaseapp.com",
+    databaseURL: "https://moneyapp-63c7a.firebaseio.com",
+    projectId: "moneyapp-63c7a",
+    storageBucket: "moneyapp-63c7a.appspot.com",
+    messagingSenderId: "684119432471",
+    appId: "1:684119432471:web:ab7614e37bfdc3082a6b52",
+    measurementId: "G-G9ZWD0PCZD"
+  },
+  webclientId: '684119432471-3r6alaao97u3i7t11c81u1t4q6ss0cb9.apps.googleusercontent.com'
 };
