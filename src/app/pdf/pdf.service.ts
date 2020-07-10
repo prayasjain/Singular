ed90030186218175cb9b34d70d9dfac342c376c0
prayasjain <prayasjain@github.com>
2020-07-10 16:44:22 +0530

    added folio number to mutual funds

--- src/app/pdf/pdf.service.ts
+++ src/app/pdf/pdf.service.ts
@@ -121,71 +121,133 @@ export class PdfService {
     let mutualFunds: MutualFunds[] = [];
     for (let i = 0; i < jsonData.length; i++) {
       let data = jsonData[i];
-      if (data.str.trim().startsWith("Registrar")) {
-        // mutual fund name is just before this
-        let fundName: string = "";
-        let j = i - 1;
-        while (
-          j >= 0 &&
-          jsonData[j].height === 7.162 &&
-          jsonData[j].fontName === "TrebuchetMS"
-        ) {
-          fundName = jsonData[j].str + fundName;
-          j = j - 1;
-        }
-        if (fundName === "") {
-          continue;
-        }
-        j = i + 1;
-        let fundPricePerUnit: number;
-        let fundPricePerUnitDate: Date; // unused
-        while (j < jsonData.length) {
-          if (
-            jsonData[j].height === 6.685 &&
-            jsonData[j].fontName === "TrebuchetMS" &&
-            jsonData[j].str.trim().startsWith("NAV on")
-          ) {
-            let split: string[] = jsonData[j].str
-              .substr("NAV on".length)
-              .replace(" ", "")
-              .split(":");
-            fundPricePerUnitDate = this.getDateFromString(split[0]);
-            let priceAndCurrency = split[split.length - 1].trim().split(" ");
-            fundPricePerUnit = +priceAndCurrency[priceAndCurrency.length - 1]
-              .trim()
-              .replace(",", "");
-            break;
-          }
-          j = j + 1;
-        }
-        j = i + 1;
-        let fundQty: number;
-        while (j < jsonData.length) {
-          if (
-            jsonData[j].height === 6.685 &&
-            jsonData[j].fontName === "TrebuchetMS" &&
-            jsonData[j].str.trim().startsWith("Closing Unit Balance")
-          ) {
-            let split: string[] = jsonData[j].str.split(":");
-            fundQty = +split[split.length - 1].trim().replace(",", "");
-            break;
-          }
-          j = j + 1;
-        }
-        if (
-          fundName !== "" &&
-          !Number.isNaN(fundPricePerUnit) &&
-          !Number.isNaN(fundQty) &&
-          fundPricePerUnitDate !== undefined
-        ) {
-          i = j;
-          mutualFunds.push(
-            new MutualFunds(fundName, fundQty, fundPricePerUnit)
-          );
-        }
+      if (!data.str.trim().startsWith("Registrar")) {
+        continue;
+      }
+      // mutual fund name is just before this
+      let fundName: string = this.getFundName(jsonData, i - 1);
+      if (fundName === "") {
+        continue;
+      }
+
+      let fundPricePerUnitAndDate = this.getFundPricePerUnitAndDate(
+        jsonData,
+        i + 1
+      );
+      let j = Math.max(i, fundPricePerUnitAndDate?.index);
+      let fundPricePerUnit: number = fundPricePerUnitAndDate?.pricePerUnit;
+      let fundPricePerUnitDate: Date = fundPricePerUnitAndDate?.date; // unused
+
+      let fundQtyAndIndex = this.getFundQty(jsonData, i + 1);
+      let fundQty: number = fundQtyAndIndex?.qty;
+      j = Math.max(j, fundQtyAndIndex?.index);
+
+      let folioNoAndIndex = this.getFolioNo(jsonData, i + 1);
+      let folioNo = folioNoAndIndex?.folioNo;
+      j = Math.max(j, folioNoAndIndex?.index);
+
+      if (
+        fundName !== "" &&
+        !Number.isNaN(fundPricePerUnit) &&
+        !Number.isNaN(fundQty) &&
+        folioNo &&
+        fundPricePerUnitDate !== undefined
+      ) {
+        i = j;
+        mutualFunds.push(
+          new MutualFunds(
+            fundName,
+            fundQty,
+            fundPricePerUnit,
+            undefined,
+            folioNo
+          )
+        );
       }
     }
     return mutualFunds;
+  }
+
+  getFundName(jsonData: CAMSJSON[], endIndex: number): string {
+    let fundName: string = "";
+    let j = endIndex;
+    while (
+      j >= 0 &&
+      jsonData[j].height === 7.162 &&
+      jsonData[j].fontName === "TrebuchetMS"
+    ) {
+      fundName = jsonData[j].str + fundName;
+      j = j - 1;
+    }
+    return fundName;
+  }
+
+  getFundPricePerUnitAndDate(
+    jsonData: CAMSJSON[],
+    startIndex: number
+  ): { pricePerUnit: number; date: Date; index: number } {
+    let j = startIndex;
+    while (j < jsonData.length) {
+      if (
+        jsonData[j].height === 6.685 &&
+        jsonData[j].fontName === "TrebuchetMS" &&
+        jsonData[j].str.trim().startsWith("NAV on")
+      ) {
+        let split: string[] = jsonData[j].str
+          .substr("NAV on".length)
+          .trim()
+          .split(":");
+        let priceAndCurrency = split[split.length - 1].trim().split(" ");
+      
+        return {
+          pricePerUnit: +priceAndCurrency[priceAndCurrency.length - 1]
+            .trim()
+            .split(',').join(""),
+          date: this.getDateFromString(split[0]),
+          index: j,
+        };
+      }
+      j = j + 1;
+    }
+  }
+
+  getFundQty(
+    jsonData: CAMSJSON[],
+    startIndex: number
+  ): { qty: number; index: number } {
+    let j = startIndex;
+    while (j < jsonData.length) {
+      if (
+        jsonData[j].height === 6.685 &&
+        jsonData[j].fontName === "TrebuchetMS" &&
+        jsonData[j].str.trim().startsWith("Closing Unit Balance")
+      ) {
+        let split: string[] = jsonData[j].str.split(":");
+        return {
+          qty: +split[split.length - 1].trim().split(',').join(""),
+          index: j,
+        };
+      }
+      j = j + 1;
+    }
+  }
+
+  getFolioNo(
+    jsonData: CAMSJSON[],
+    startIndex: number
+  ): { folioNo: string; index: number } {
+    let j = startIndex;
+    while (j < jsonData.length) {
+      if (
+        jsonData[j].height === 7.64 &&
+        jsonData[j].fontName === "TrebuchetMS" &&
+        jsonData[j].str.trim().startsWith("Folio No")
+      ) {
+        let split: string[] = jsonData[j].str.split(":");
+        return { folioNo: split[split.length - 1].trim(), index: j };
+      }
+      j = j + 1;
+    }
   }
 
   parseCAMSFormat2(jsonData: CAMSJSON[]): MutualFunds[] {
@@ -240,7 +302,7 @@ export class PdfService {
           ) {
             let split: string[] = jsonData[j].str
               .substr("NAV on".length)
-              .replace(" ", "")
+              .split(' ').join("")
               .split(":");
             if (moment(split[0].trim(), "DD-MMM-yyyy").isValid()) {
               fundPricePerUnitDate = moment(
@@ -251,7 +313,7 @@ export class PdfService {
             let priceAndCurrency = split[split.length - 1].trim().split(" ");
             fundPricePerUnit = +priceAndCurrency[priceAndCurrency.length - 1]
               .trim()
-              .replace(",", "");
+              .split(',').join("");
             break;
           }
           j = j + 1;
@@ -265,7 +327,7 @@ export class PdfService {
             jsonData[j].str.trim().startsWith("Closing Unit Balance")
           ) {
             let split: string[] = jsonData[j].str.split(":");
-            fundQty = +split[split.length - 1].trim().replace(",", "");
+            fundQty = +split[split.length - 1].trim().split(',').join("");
             break;
           }
           j = j + 1;
@@ -360,9 +422,14 @@ export class PdfService {
       if (oldAccount.assetType !== AssetType.MutualFunds) {
         continue;
       }
+      let oldFolioNo: string = oldAccount.mutualFunds.folioNo;
+      let newFolioNo: string = mutualFund.folioNo;
+      if (!oldFolioNo || !newFolioNo) {
+        continue;
+      }
       if (
-        oldAccount.mutualFunds.fundName.split("-")[0].trim() ===
-        mutualFund.fundName.split("-")[0].trim()
+        oldFolioNo.trim().split(" ").join("") ===
+        newFolioNo.trim().split(" ").join("")
       ) {
         return oldAccount;
       }
