086de3ded5359242e2385f19f5f2f27ccfdb6b0f
himanshiSehrawat-kiwi <himanshi.sehrawat@kiwitech.com>
2020-08-13 14:26:57 +0530

    removed extra variables

--- src/app/modules/dashboard/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard/dashboard.component.ts
@@ -1,23 +1,7 @@
 import { Component, OnInit, ViewChild, AfterViewInit, ViewEncapsulation } from '@angular/core';
 import { AssetsPage } from 'src/app/home/assets/assets.page';
-import { AssetsService } from 'src/app/home/assets/assets.service';
-import { AuthService } from 'src/app/auth/auth.service';
-import { LoadingController } from '@ionic/angular';
-import { CurrencyService } from 'src/app/home/currency/currency.service';
-import { MarketDataService } from 'src/app/home/assets/market-data.service';
-import { Asset, AssetType } from 'src/app/home/assets/asset.model';
-import { Subscription, zip, of } from 'rxjs';
-import { take, switchMap, tap } from 'rxjs/operators';
 import { GoalsPage } from 'src/app/home/goals/goals.page';
-import { GoalsService } from 'src/app/home/goals/goals.service';
 
-
-interface AssetGroup {
-  assetType: AssetType;
-  amount: number;
-  // id included to identify eash list item seprately
-  id: number;
-}
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
