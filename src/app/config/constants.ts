6191518a4d19fcf5fa1593224cffc7ca44a7cf97
himanshiSehrawat-kiwi <himanshi.sehrawat@kiwitech.com>
2020-08-27 22:36:48 +0530

    Graph Implementation

--- src/app/config/constants.ts
+++ src/app/config/constants.ts
@@ -63,6 +63,20 @@ export class Constants {
         ONE_MONTH: 'ONE_MONTH',
         WEEKLY: 'WEEKLY'
     };
+    public static readonly MONTHS = {
+        0: 'Jan',
+        1: 'Feb',
+        2: 'Mar',
+        3: 'Apr',
+        4: 'May',
+        5: 'Jun',
+        6: 'Jul',
+        7: 'Aug',
+        8: 'Sept',
+        9: 'Oct',
+        10: 'Nov',
+        11: 'Dec'
+    };
 
     public static readonly ICON_TYPES = {
         'Mutual Funds': 'funds.svg',
